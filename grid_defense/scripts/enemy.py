38937e6e51e9406b0241c993b2fb2913624d4df9
Lucas Walter <wsacul@gmail.com>
2016-02-06 07:47:00 -0800

    Now drawing marker, but will need to turn this into a marker array inside enemy manager so rviz doesn't have to be manually configured to display all of them.

--- grid_defense/scripts/enemy.py
+++ grid_defense/scripts/enemy.py
@@ -7,6 +7,8 @@
 import rospy
 import tf
 
+from visualization_msgs.msg import Marker
+
 class Enemy:
     def __init__(self):
         self.br = tf.TransformBroadcaster()
@@ -17,12 +19,23 @@ class Enemy:
         self.link = name.replace(namespace, "", 1)
         rospy.loginfo("link \"" + self.link + "\"")
 
+        self.marker = Marker()
+        self.marker.header.frame_id = self.link
+        self.marker.type = self.marker.CUBE
+        self.marker.scale.x = 0.8
+        self.marker.scale.y = 0.8
+        self.marker.scale.z = 0.8
+        self.marker.color.a = 1.0
+        self.marker_pub = rospy.Publisher("marker", Marker, queue_size = 1)
+
         # how fast the unit advances
         self.seconds_per_square = rospy.get_param("seconds_per_square", 6)
         self.vel = -1.0 / self.seconds_per_square
 
-        self.x = rospy.get_param("~x", 10)
-        self.y = rospy.get_param("~y", 1)
+        self.init_x = rospy.get_param("~x", 10)
+        self.x = self.init_x
+        self.init_y = rospy.get_param("~y", 1)
+        self.y = self.init_y
         self.z = 0
 
         self.dt = 0.05
@@ -30,11 +43,15 @@ class Enemy:
 
     def update(self, event):
         self.x += self.vel * self.dt
+        if self.x < 0:
+           self.x = self.init_x
         self.br.sendTransform((self.x, self.y, self.z),
                               (0, 0, 0, 1.0),
                               rospy.Time.now(),
                               self.link,
                               "map")
+        # TODO(lucasw) Do I need to keep publishing this over and over?
+        self.marker_pub.publish(self.marker)
 if __name__ == '__main__':
     rospy.init_node('enemy')
     enemy = Enemy()
