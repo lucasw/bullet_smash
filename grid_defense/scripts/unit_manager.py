752eb78a8446d5b27b1846bf70f05da8154b8ee1
Lucas Walter <wsacul@gmail.com>
2016-02-20 05:43:58 -0800

    The unit manager may need to be renamed to collision detector.  Can the tf tree be parsed easily, or should the lists of units be broadcast otherwise and received here?  It would sort of be like tf, but only for certain types of units.

---
+++ grid_defense/scripts/unit_manager.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# Lucas Walter
+
+# This needs to be able to get a list of the tf of every enemy,
+# and every projectile, and later every defender unit, and
+# detect collisions between them, and cause them to take damage
+# after a collision
